4123452055971a1e4f1dee475281b9d86219e4ed
hc65083 <hc65083@uga.edu>
2026-09-14 10:20:48 -0400

    d

---
+++ script.js
@@ -0,0 +1,29 @@
+const MBTIBuilds = [
+    {
+        name: "mae",
+        mbti:""
+    },
+
+    {
+        
+    }
+];
+
+const zodiacBuilds = [
+    {
+        name: "wiley",
+        zodiac:""
+    },
+
+    {
+        
+    }
+];
+
+function hi(params) {
+    //hi
+}
+
+for (let i = 0; i < MBTIBuilds.length; i++) {
+    //hi
+}
